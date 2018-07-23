89e2081d5875f624d90d1195700db9748442d21e
Jennifer Hansen <jenn.leigh.hansen@gmail.com>
2018-07-23 17:57:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ function setThisWithCall(fn, thisValue, arg) {
   return fn.call(thisValue, arg)
 }
 
-function setThisWithCall(fn, thisValue, arg) {
-  return fn.call(thisValue, arg)
+function setThisWithApply(fn, thisValue, arg) {
+  return fn.apply(thisValue, [arg])
 }
